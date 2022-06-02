4e27f3c66566d1d4c9413b5a20693adb25d41ebc
Cryxile <creeper.super99@gmai.com>
2022-06-02 22:59:51 +0300

    Added array creation method

--- ArrayProject.java
+++ ArrayProject.java
@@ -1,29 +1,41 @@
-import java.util.*;
-
+import java.util.Random;
 
 public class ArrayProject {
 
-    public static void main(String[] args) {
-
-        InputWithCheck inputWithCheck = new InputWithCheck();
-        int arrayDimensionValue = inputWithCheck.readAndCheckDimensionValue();
-        int arrayMinRandomValue = inputWithCheck.readAndCheckMinRandomValueOfArray();
-        int arrayMaxRandomValue = inputWithCheck.readAndCheckMaxRandomValueOfArray();
-        String arrayPrintQuestion = inputWithCheck.readAndCheckPrintQuestionOfArray();
+    public static int[] arrayCreation(int arrayDimensionValue, int arrayMinRandomValue, int arrayMaxRandomValue) {
 
         Random rand = new Random();
         int[] randomValues = new int[arrayDimensionValue];
         for (int i = 0; i < arrayDimensionValue; i++) {                                    // Заполнение массива случайными числами
             randomValues[i] = Math.round(rand.nextInt(arrayMinRandomValue, arrayMaxRandomValue + 1));
         }
+        return randomValues;
+    }
+
+    public static void main(String[] args) {
+
+        InputWithCheck inputWithCheck = new InputWithCheck();
+
+        System.out.print("Enter the dimension value of the array: ");
+        int arrayDimensionValue = inputWithCheck.readAndCheckDimensionValue();
+
+        System.out.print("Enter the minimal random value: ");
+        int arrayMinRandomValue = inputWithCheck.readAndCheckMinAndMaxRandomValue();
+
+        System.out.print("Enter the maximum random value: ");
+        int arrayMaxRandomValue = inputWithCheck.readAndCheckMinAndMaxRandomValue();
+
+        System.out.print("Do you want to print arrays to console? (y/n) ");
+        String arrayPrintQuestion = inputWithCheck.readAndCheckPrintQuestionOfArray();
+
+
+        int[] randomValues = arrayCreation(arrayDimensionValue, arrayMinRandomValue, arrayMaxRandomValue);
 
         OperationsWithArray operationsWithArray = new OperationsWithArray();
 
         if (arrayPrintQuestion.equals("y")) {                                              // Вывод значений массива в консоль
 
             operationsWithArray.printArray(randomValues);
-
-            operationsWithArray.countOddAndEvenValuesOfArray(randomValues);
 
             operationsWithArray.printEvenValuesOfArray(randomValues);
 
@@ -37,7 +49,6 @@ public class ArrayProject {
 
             operationsWithArray.printDescendingBubbleSortedArray(randomValues);
         }
-
         operationsWithArray.printMinMaxAverageValuesOfArray(randomValues);
     }
 }
